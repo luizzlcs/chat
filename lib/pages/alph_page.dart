96d93e031679973769293d45a40b3d87223b19a3
Luiz Carlos <luizzlcs@hotmail.com>
2022-09-02 12:17:11 -0300

    Recebendo comfirmação do Loadin da paga auth_form. construído circularProgress com containe transparente.

--- lib/pages/alph_page.dart
+++ lib/pages/alph_page.dart
@@ -1,17 +1,52 @@
 import 'package:chat/components/auth_form.dart';
+import 'package:chat/model/auth_form_data.dart';
 import 'package:flutter/material.dart';
 
-class AlphPage extends StatelessWidget {
+class AlphPage extends StatefulWidget {
   const AlphPage({Key? key}) : super(key: key);
 
   @override
+  State<AlphPage> createState() => _AlphPageState();
+}
+
+class _AlphPageState extends State<AlphPage> {
+  bool _isLoading = false;
+
+  void _handleSubmit(AuthFormData formData) {
+    setState(() => _isLoading = true);
+    print('AuthPage...');
+    print(formData.email);
+
+    // setState(() => _isLoading = false);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).primaryColor,
-      body: const Center(
-        child: SingleChildScrollView(
-          child: AuthForm(),
-        ),
+    return GestureDetector(
+      onTap: () {
+        FocusScopeNode currentFocus = FocusScope.of(context);
+        if (!currentFocus.hasPrimaryFocus) {
+          currentFocus.unfocus();
+        }
+      },
+      child: Scaffold(
+        backgroundColor: Theme.of(context).primaryColor,
+        body: Stack(children: [
+          Center(
+            child: SingleChildScrollView(
+              child: AuthForm(onSubmit: _handleSubmit),
+            ),
+          ),
+          if (_isLoading)
+            Container(
+              decoration: const BoxDecoration(
+                color: Color.fromRGBO(0, 0, 0, 0.5),
+              ),
+              child: const Center(
+                child: CircularProgressIndicator(),
+              ),
+            )
+        ]),
       ),
     );
   }
